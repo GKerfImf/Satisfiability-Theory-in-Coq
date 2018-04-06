56d0ddda8604238c00a21ca07258d361cbab267e
Bozhko Sergey <gkerfimf@gmail.com>
2018-04-06 15:24:03 +0300

    Make some progress with Traversable

--- TypeClasses.v
+++ TypeClasses.v
@@ -1,6 +1,9 @@
 Require Import Coq.Init.Datatypes.
 Set Implicit Arguments.
 
+Require Import List.
+Import ListNotations.
+
 Module TypeClasses.
 
   Definition id {A} (x: A) := x.
@@ -12,7 +15,13 @@ Module TypeClasses.
   Reserved Notation "f [=] g" (at level 70, no associativity).
   Definition extensional_equivalence {A B} (f g: A -> B) := forall x, f x = g x.
   Infix "[=]" := extensional_equivalence.
-  
+
+  Inductive identity {A: Type}: Type := Ident: A -> @identity A.
+  (* Inductive composition {F: Type -> Type} {G: Type -> Type} := Comp: forall X, X -> omposition F G. *)
+
+(*  Print option.
+  Print identity.
+  *)
   (*  Notation ...
   Definition compose f g = ... *)
   
@@ -53,7 +62,7 @@ Module TypeClasses.
     Class Applicative (F: Type -> Type) :=
       { functor :> Functor F
         ; pure: forall {A}, A -> F A
-        ; app: forall {A B}, F (A -> B) -> F A -> F B where "n <*> m" := (app n m)
+        ; app: forall {A B: Type}, F (A -> B) -> F A -> F B where "n <*> m" := (app n m)
         ; _: forall {A} (v: F A), pure id <*> v = v
         ; _: forall {A B} (f: A -> B) (x: A), pure f <*> pure x = pure (f x)
         ; _: forall {A B} (u: F(A -> B)) (y: A),
@@ -81,7 +90,7 @@ Module TypeClasses.
     
   End Monad.
 
-  Section Transformers.
+(*  Section Transformers.
 
     Class MonadTrans (M: Type -> Type) (T: _) :=
       { monadM :> Monad M
@@ -92,10 +101,86 @@ Module TypeClasses.
       }.
 
     Instance j : (MonadTrans 2) := n.
-  End Transformers.
-  
-  Section Option.
     
+  End Transformers. *)
+
+  Section Monoid.
+
+    Class Monoid (M: Type) :=
+      { mempty: M
+        ; mappend: M -> M -> M
+        ; _: forall x, mappend mempty x = x
+        ; _: forall x, mappend x mempty = x
+        ; _: forall x y z, mappend (mappend x y) z = mappend x (mappend y z)
+      }.
+        
+  End Monoid.
+  
+  Section Foldable.
+
+    Class Foldable (T: Type -> Type) :=
+      { foldMap: forall {M} `{! Monoid M} a,  (a -> M) -> T a -> M }.
+    
+  End Foldable.
+  
+  Section Traversable.
+
+    Class ApplicativeTransformation
+          {T} `{! Applicative T}
+          {F} `{! Applicative F} (t: forall {A}, T A -> F A) :=
+      { 
+         _: forall {A} (x: A),
+          t (pure x) = pure x
+        ; _: forall {A B: Type} (x:_ ) (y: _),
+            t (app x y) = @app F _ A B (t x) (t y)
+      }.
+ 
+
+    Instance identity_functor: (Functor (@identity)) :=
+      { fmap A B f a := 
+          match a with
+          | Ident a => Ident (f a)
+          end
+      }.
+    Proof.
+      { intros A a; destruct a; reflexivity. }
+      { intros ? ? ? ? ? ?; destruct x; reflexivity. }
+    Defined.
+
+    Instance identity_applicative: (Applicative (@identity)) :=
+      { pure A a := Ident a
+        ; app A B f a :=
+            match f, a with
+            | Ident f, Ident a => Ident (f a)
+            end
+      }.
+    Proof.
+      { intros; destruct v; reflexivity. } 
+      { intros; reflexivity. }
+      { intros; destruct u; reflexivity.  }
+      { intros; destruct u, v, w; reflexivity. }
+    Defined.    
+      
+  
+     Class Traversable (T: Type -> Type) :=
+      { functorTraversable :> Functor T 
+        ; foldableTraversable :> Foldable T
+        ; traverse: forall {A} {B} {F} `{! Applicative F},
+            (A -> F B) -> T A -> F (T B)
+        (* ; _: traverse (Compose.fmap g . f) =Compose . fmap (traverse g) . traverse f *)
+
+        ; _: forall (A B: Type) (F: Type -> Type) `{Applicative F} 
+                    (f: A -> F B) (t: forall {A}, F A -> F A)
+                    `{! ApplicativeTransformation (@t)},
+            (* `{! ApplicativeTransformation t} *)
+            
+            ((t (.) (traverse f))) [=] ((traverse (t (.) f))) 
+
+        ; _: forall {A: Type}, (@traverse A _ _ _ (Ident)) [=] Ident
+        ; _: True
+      }.
+
+
     Instance option_functor: (Functor option) :=
       { fmap A B f a :=
           match a with
@@ -107,7 +192,21 @@ Module TypeClasses.
       intros. intros x. destruct x; reflexivity.
       intros. intros x. 
       compute. destruct x; reflexivity.
-      
+    Defined.
+
+    Instance list_functor: (Functor list) :=
+      { fmap A B f a := map f a }.
+    Proof.
+      { intros A xs.
+        induction xs.
+        - reflexivity.
+        - simpl; rewrite IHxs; compute; reflexivity.
+      }
+      { intros ? ? ? ? ? xs; unfold compose.
+        induction xs.
+        - reflexivity.
+        - simpl; rewrite IHxs; compute; reflexivity.
+      }
     Defined.
     
     Instance option_applicative: (Applicative option) :=
@@ -123,8 +222,31 @@ Module TypeClasses.
       - intros; reflexivity. 
       - intros; destruct u; reflexivity.
       - intros; destruct u, v, w; reflexivity.
-    Qed.
-
+    Defined.
+    
+    Instance option_traversable: (Traversable option) :=
+      { traverse {A B} {F} _ (f: A -> F B) (some: option A) := 
+          match some with
+          | None => pure None : F (option B)
+          | Some x => app (pure (Some ) ) (f x) : F (option B)
+          end
+      }.
+    Proof.
+      { admit. }
+      { intros ? ? ? ? ? ? ? ?; destruct x; unfold compose.
+        - destruct ApplicativeTransformation0.
+          rewrite H1, H0; reflexivity. 
+        - destruct ApplicativeTransformation0; eauto.
+      }
+      { intros ? ?; destruct x; reflexivity. } 
+      {  }
+    Defined.
+       
+  End Traversable.
+  
+  Section Option.
+    
+    
     Instance option_monad: (Monad option) :=
       { unit A a := Some a;
         bind A B a f :=
