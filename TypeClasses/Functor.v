4eb1c9a50ba3de0182f9d576761268cf3e870365
Bozhko Sergey <gkerfimf@gmail.com>
2018-04-08 02:27:38 +0300

    Prove functor_prop lemma

--- TypeClasses/Functor.v
+++ TypeClasses/Functor.v
@@ -23,23 +23,32 @@ Module Functor.
       { fmap: forall {A B}, (A -> B) -> (F A -> F B)
         ; fmap_id: forall {A}, @fmap A _ id [=] id
         ; fmap_comp: forall {A B C} (f: B -> C) (g: A -> B),
-            fmap (f(.) g) [=] ((fmap f) (.) (fmap g))
+            fmap (f (.) g) [=] ((fmap f) (.) (fmap g))
       }.
 
   End Definitions.
 
   Section Lemmas.
-
+    
+    Axiom EE: forall A B (f g: A -> B),
+      (forall x, f x = g x) -> f = g.
+                                          
     Lemma functor_property:
       forall (F: Type -> Type) `{! Functor F} (G: Type -> Type) `{! Functor G}
              (A B C: Type) (f: B -> C) (g: A -> B) (x: F (G A)),
         fmap (fmap (f (.) g)) x = fmap (fmap f) (fmap (fmap g) x).
-    Proof.
-      clear.
+    Proof. 
       intros.
-      admit.
-      
-    Admitted.
+      assert (Eq: fmap (f (.) g) = (fmap f (.) fmap g)).
+      { clear x.
+        destruct Functor1; unfold fmap.
+        apply EE; intros x; apply fmap_comp0.
+      } rewrite Eq; clear Eq.
+      destruct Functor0; unfold fmap at 1.
+      assert (Eq := fmap_comp0 _ _ _ (fmap f) (fmap g) x).
+      rewrite Eq; clear Eq.
+      reflexivity.
+    Qed.
 
   End Lemmas.
   
