17badd2d0e01719711c8d2195af9dcc17ba90e50
Sergei Bozhko <sbozhko@mpi-sws.org>
2019-08-28 12:58:36 +0200

    Create a new file

--- project3.v
+++ project3.v
@@ -1,72 +1,10 @@
 Require Export List Omega.
 Import ListNotations.
- 
-(** * Feed tactic. *)
 
-(* This tactic feeds the precondition of an implication in order to derive the conclusion
-   (taken from http://comments.gmane.org/gmane.science.mathematics.logic.coq.club/7013).
+Require Export icl.util.
 
-   Usage: feed H.
 
-   H: P -> Q  ==becomes==>  H: P
-                            ____
-                            Q
 
-   After completing this proof, Q becomes a hypothesis in the context. *)
-Ltac feed H :=
-  match type of H with
-  | ?foo -> _ =>
-    let FOO := fresh in
-    assert foo as FOO; [|specialize (H FOO); clear FOO]
-  end.
-
-(* Generalization of feed for multiple hypotheses.
-   feed_n is useful for accessing conclusions of long implications.
-
-   Usage: feed_n 3 H.
-     H: P1 -> P2 -> P3 -> Q.
-
-   We'll be asked to prove P1, P2 and P3, so that Q can be inferred. *)
-Ltac feed_n n H := match constr:(n) with
-  | O => idtac
-  | (S ?m) => feed H ; [| feed_n m H]
-  end.
-
-(** Smolka's *)
-
-Definition dec X := {X} + {~X}.
-Notation "'eq_dec' X" := (forall x y: X, dec (x = y)) (at level 70).
-
-Notation "x 'el' A" := (In x A) (at level 70).
-Notation "x 'nel' A" := (~ In x A) (at level 70).
-
-Existing Class dec.
-Definition decision (X : Prop) (D : dec X) : dec X := D.
-Arguments decision X {D}.
-
-Tactic Notation "decide" constr(p) := 
-  destruct (decision p).
-Tactic Notation "decide" constr(p) "as" simple_intropattern(i) := 
-  destruct (decision p) as i.
-
-Lemma size_recursion (X: Type) (σ: X -> nat) (p: X -> Type):
-  (forall x, (forall y, σ y < σ x -> p y) -> p x) -> 
-  forall x, p x.
-Proof.
-  intros D x. apply D.
-  cut (forall n y, σ y < n -> p y).
-  now eauto.
-  clear x. intros n.
-  induction n; intros y E.
-  - exfalso. omega. 
-  - apply D. intros x F. apply IHn. omega.
-Defined.
-
-Instance nat_eq_dec: 
-  eq_dec nat.
-Proof.
-  intros x y. hnf. decide equality.
-Defined.
 
 (** Qed. ~> Defined. *)
 
@@ -142,10 +80,7 @@ Defined. (* Todo: Qed? *)
 
 (** * ??? *)
 
-Definition equi {X: Type} (A B: list X) : Prop :=
-  incl A B /\ incl B A.
 
-Hint Unfold equi.
 
 Lemma inclusion_app {T: Type} (xs1 xs2 xs: list T): 
   incl (xs1 ++ xs2) xs ->
@@ -164,40 +99,6 @@ Proof.
       eauto.
 Defined. (* Todo: Qed? *)
 
-
-Instance list_in_dec {T: Type} (x: T) (xs: list T): 
-  eq_dec T -> dec (x el xs).
-Proof.
-  intros D; apply in_dec; exact D.
-Defined. (* Todo: Qed? *)
-
-
-Instance inclusion_dec {T: Type} (xs1 xs2: list T):
-  eq_dec T -> dec (incl xs1 xs2).
-Proof.
-  intros D.
-  induction xs1.
-  { left.
-    intros x IN; inversion IN. }
-  { destruct IHxs1 as [INCL|NINCL].
-    decide (a el xs2) as [IN|NIN].
-    { left; intros x IN'.
-      destruct IN'.
-      - subst x; assumption.
-      - specialize (INCL x); auto. } 
-    { right.
-      intros CONTR.
-      apply NIN, CONTR.
-      left; reflexivity.
-    }
-    { right.
-      intros CONTR; apply NINCL; clear NINCL.
-      intros x IN.
-      apply CONTR.
-      right; assumption.
-    }
-  }
-Defined. (* Todo: Qed? *)
 
 
 
